4fa3e786a8c16d5479ce66b733b175bf7fcbe505
Diogo Deconto <dideconto@gmail.com>
2021-03-12 19:39:02 -0300

    Configuração do respositório

---
+++ Ambientacao/src/Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner - Leitura dos dados do teclado
+		
+		System.out.println("Boa noite para todos! Bom final de semana!");
+	}
+
+}
